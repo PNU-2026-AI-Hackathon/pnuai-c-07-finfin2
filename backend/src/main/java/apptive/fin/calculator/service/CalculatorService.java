3e3120185c6a6b985528b7b0b7172f2c1766c5d4
zwo-n <kjw497@naver.com>
2026-07-06 18:11:00 +0900

    fix: 클래스 오류 수정

--- backend/src/main/java/apptive/fin/calculator/service/CalculatorService.java
+++ backend/src/main/java/apptive/fin/calculator/service/CalculatorService.java
@@ -1,5 +1,8 @@
 package apptive.fin.calculator.service;
 
+import apptive.fin.calculator.dto.CalculatorRequestDto;
+import apptive.fin.calculator.dto.CalculatorResponseDto;
+import apptive.fin.search.ProductType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +12,13 @@ public class CalculatorService {
 
     private final RateCalculatorFactory calculatorFactory;
 
-    public CalculatorResponse simulate(CalculatorRequest request) {
+    public CalculatorResponseDto simulate(CalculatorRequestDto request) {
         validate(request);
         RateCalculator calculator = calculatorFactory.getCalculator(request.productType());
         return calculator.calculate(request);
     }
 
-    private void validate(CalculatorRequest request) {
+    private void validate(CalculatorRequestDto request) {
         if (request.productType() == ProductType.SAVING && request.reserveType() == null) {
             throw new IllegalArgumentException("적금 계산기는 reserveType(적립 방식: 정액 또는 자유)이 필요합니다.");
         }
