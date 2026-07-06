29cfcd64d7f04919dbf9d876ec19e6eb6ea3653b
zwo-n <kjw497@naver.com>
2026-07-06 18:06:56 +0900

    feat: implement Calculator Service

--- backend/src/main/java/apptive/fin/calculator/service/CalculatorService.java
+++ backend/src/main/java/apptive/fin/calculator/service/CalculatorService.java
@@ -6,4 +6,19 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CalculatorService {
+
+    private final RateCalculatorFactory calculatorFactory;
+
+    public CalculatorResponse simulate(CalculatorRequest request) {
+        validate(request);
+        RateCalculator calculator = calculatorFactory.getCalculator(request.productType());
+        return calculator.calculate(request);
+    }
+
+    private void validate(CalculatorRequest request) {
+        if (request.productType() == ProductType.SAVING && request.reserveType() == null) {
+            throw new IllegalArgumentException("적금 계산기는 reserveType(적립 방식: 정액 또는 자유)이 필요합니다.");
+        }
+
+    }
 }
